9f05cb1e1de8e383eb44589894bcee33063d6a34
AbdushukurUrinboev <urinboevabdushukur@gmail.com>
2023-03-07 15:57:01 +0500

    foo

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/posts')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+
+  return (
+    <div className='container mt-5'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App;
